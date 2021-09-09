0fc1ba411db2ebbb6ac44a388456d9a39bdd6416
Александр Веселов <598746@gmail.com>
2021-09-09 21:31:54 +0400

    2

---
+++ TextGenerator/Models/TextParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextGenerator.Models
+{
+    public class TextParams
+    {
+        public int Length { get; set; } = 100;
+        public double Temperature { get; set; } = 1.0;
+        public int K { get; set; } = 10;
+        public double P { get; set; } = 0.9;
+        public double RepetitionPenalty { get; set; } = 1.0;
+        public int NumReturnSequences { get; set; } = 1;
+
+
+    }
+}
--- TextGenerator/PythonNetWorker.cs
+++ TextGenerator/PythonNetWorker.cs
@@ -49,7 +49,35 @@ namespace TextGenerator
             }
         }
 
-        private void SetPaths(string pathPythonFolder) {
+        /// <summary>
+        /// Генерирует текст на английском языке.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="pathPythonFolder"> сюда нужно передать путь до папки в которой хранится скрипт</param>
+        /// <returns></returns>
+        public string GenerateRusText(string text, string pathPythonFolder) {
+            SetPaths(pathPythonFolder); // сюда нужно передать путь до папки со скриптом
+            using (Py.GIL()) 
+            {
+                try
+                {
+                    dynamic sampleModule = Py.Import("text_expansion"); // сюда нужно передать название скрипта
+                    dynamic setParams = sampleModule.SetParams(/*length =*/ 100, /*temperature =*/ 1.0, /*k = */  10, /*p*/  0.9, /*repetition_penalty*/  1.0, /*num_return_sequences*/  1); // вызов метода из скрипта
+                    dynamic result = sampleModule.paraphrase_and_expand_text(text, true, true); // вызов метода из скрипта
+                    return result;
+                }
+                catch (PythonException error)
+                {
+                    // Communicate errors with exceptions from within python script -
+                    // this works very nice with pythonnet.
+                    Console.WriteLine("Error occured: ", error);
+                    return null;
+                }
+            }
+        }
+
+        private void SetPaths(string pathPythonFolder)
+        {
             // Setup all paths before initializing Python engine
             string pathToPython = _pathToPythonDirectory;
             string path = pathToPython + ";" +
@@ -66,35 +94,6 @@ namespace TextGenerator
 
             string paths = string.Join(";", lib);
             Environment.SetEnvironmentVariable("PYTHONPATH", paths, EnvironmentVariableTarget.Process);
-
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="text"></param>
-        /// <param name="pathPythonFolder"> сюда нужно передать путь до папки в которой хранится скрипт</param>
-        /// <returns></returns>
-        public string GenerateRusText(string text, string pathPythonFolder) {
-            SetPaths(pathPythonFolder); // сюда нужно передать путь до папки со скриптом
-            using (Py.GIL()) 
-            {
-                try
-                {
-                    dynamic sampleModule = Py.Import("text_expansion"); // сюда нужно передать название скрипта
-                    dynamic setParams = sampleModule.SetParams(/*length =*/ 100, /*temperature =*/ 1.0, /*k = */  10, /*p*/  0.9, /*repetition_penalty*/  1.0, /*num_return_sequences*/  1); // вызов метода из скрипта
-                    dynamic result = sampleModule.paraphrase_and_expand_text(text, true, true); // вызов метода из скрипта
-                    return result;
-
-                }
-                catch (PythonException error)
-                {
-                    // Communicate errors with exceptions from within python script -
-                    // this works very nice with pythonnet.
-                    Console.WriteLine("Error occured: ", error);
-                    return null;
-                }
-            }
 
         }
     }
