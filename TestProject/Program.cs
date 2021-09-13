e42034feed0aa5b3ff24404989e8ea1e580fc4bf
evgeniySorokin97598 <79045246+evgeniySorokin97598@users.noreply.github.com>
2021-09-13 13:25:38 +0400

    1

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -24,7 +24,7 @@ namespace TestProject
             string path = @"C:\Users\Admin\Desktop\нейросетка\TextGenerator2\TextGenerator\";
             //worker.DownloadPackages(path);
             worker.SaveScripts();
-            //worker.DownloadPackages();
+            worker.DownloadPackages();
             worker.DownloadModels();
             
         }
